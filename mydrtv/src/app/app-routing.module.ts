0bb599027ddfd76b41d07a5885dc31d9d1d83622
Michal P <michalpawlicki@outlook.com>
2019-05-03 07:59:31 +0200

    player imported. route created

--- mydrtv/src/app/app-routing.module.ts
+++ mydrtv/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {PlayerComponent} from "./player/player.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'app', pathMatch: 'full'},
+
+  {
+    path: 'player',
+    component: PlayerComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- mydrtv/src/app/app.module.ts
+++ mydrtv/src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Video player import starts
+import {VgCoreModule} from 'videogular2/core';
+import {VgControlsModule} from 'videogular2/controls';
+import {VgOverlayPlayModule} from 'videogular2/overlay-play';
+import {VgBufferingModule} from 'videogular2/buffering';
+// Video player import ends
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PlayerComponent } from './player/player.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlayerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    VgCoreModule,
+    VgControlsModule,
+    VgOverlayPlayModule,
+    VgBufferingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
