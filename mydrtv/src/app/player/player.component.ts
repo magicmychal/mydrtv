f42346c6b2a8a2339d00be911e187d6b0cd71627
Michal P <michalpawlicki@outlook.com>
2019-05-13 14:48:58 +0200

    basic http request for single movie

--- mydrtv/src/app/player/player.component.ts
+++ mydrtv/src/app/player/player.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Injectable } from '@angular/core';
 import {Store} from '@ngrx/store';
 import {FilmModel} from '../models/film.model';
 import {Observable} from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-player',
@@ -11,12 +14,22 @@ import {Observable} from 'rxjs';
 export class PlayerComponent implements OnInit {
   films: Observable<{films: FilmModel[]}>;
 
-  constructor(private store: Store<{films: {films: FilmModel[]} }>) { }
+  constructor(
+    private store: Store<{films: {films: FilmModel[]} }>,
+    private http: HttpClient
+  ) { }
 
   ngOnInit() {
     console.log('on init');
     this.films = this.store.select('films');
     console.log(this.films);
+    console.log('load movie here');
+    this.getMovie('5cd9655d19fcad52cc9bb9ad');
   }
 
+  getMovie(filmId: string): any {
+    let apiURL = 'http://localhost:3000/films/';
+    console.log('GET' + filmId);
+    this.http.get(apiURL + '/' + filmId).subscribe(res => console.log(res['Actors']));
+  }
 }
