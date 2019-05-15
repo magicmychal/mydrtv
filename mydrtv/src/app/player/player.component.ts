07c9ebb8e5b8411899aff90da2a3044dd19fb690
Michal P <michalpawlicki@outlook.com>
2019-05-15 17:51:47 +0200

    redirect if not found

--- mydrtv/src/app/player/player.component.ts
+++ mydrtv/src/app/player/player.component.ts
@@ -1,7 +1,6 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FilmRestService} from '../services/film-rest.service';
-import { ActivatedRoute, Router } from '@angular/router';
-import {error} from "selenium-webdriver";
+import {ActivatedRoute, Router} from '@angular/router';
 
 
 @Component({
@@ -13,11 +12,14 @@ export class PlayerComponent implements OnInit {
   public filmId: string;
   films: any = [];
   film: any;
+  notFound: string;
+
   constructor(
     public rest: FilmRestService,
     private route: ActivatedRoute,
     private router: Router
-  ) { }
+  ) {
+  }
 
   ngOnInit() {
     //this.getMovie();
@@ -29,20 +31,24 @@ export class PlayerComponent implements OnInit {
     //   });
     this.rest.getMovie(this.filmId).subscribe({
       next: x => this.film = x,
-      error: err => console.error(err),
+      error: err => this.filmNotFound(),
       complete: () => console.log('done')
     });
   }
 
+  // if the film was not found in the database
+  // user is redirected to the homepage
   filmNotFound() {
-
+    this.notFound = 'Movie not found. You will be redirected to the main page in a moment...';
+    setTimeout(() => {
+      this.router.navigate(['/']);
+    }, 3000);  //3s
   }
 
   getMovie() {
     this.films = [];
     this.rest.getMovie('5cd9655d19fcad52cc9bb9ad').subscribe((data: {}) => {
       console.log(data);
-      console.log('huj mi w pupe');
       this.films = data;
     });
   }
