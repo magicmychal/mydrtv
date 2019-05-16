20cd51b45487843b54d0c1d913a72450c93faaad
cjullin <caro2715@stud.kea.dk>
2019-05-16 14:04:34 +0200

    signup setup without db connection

--- mydrtv/src/app/signup/signup.component.ts
+++ mydrtv/src/app/signup/signup.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +10,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  signupForm: FormGroup;
+
+  constructor(private fb: FormBuilder, private router: Router) {}
 
   ngOnInit() {
+    this.signupForm = this.fb.group({
+        name: ['', [Validators.required, Validators.minLength(3)]],
+        lastName: ['', [Validators.required, Validators.minLength(3)]],
+        email: ['', [Validators.required, Validators.minLength(3)]],
+        password: ['', [Validators.required, Validators.minLength(3)]],
+        gender: ['', [Validators.required, Validators.minLength(1)]]
+      })
+  }
+
+  onSubmit() {
+    console.log(this.signupForm.value);
   }
 
 }
+
