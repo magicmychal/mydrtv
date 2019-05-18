4a666c8672eb760cf65b7996291996ea40d3d66a
Renata Rebeca Gavreliuc <renata_girl95@yahoo.com>
2019-05-18 09:20:55 +0200

    db connection on home

--- mydrtv/src/app/services/get-movies.service.ts
+++ mydrtv/src/app/services/get-movies.service.ts
@@ -3,11 +3,17 @@ import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Observable, of} from 'rxjs';
 import {catchError, map, tap} from 'rxjs/operators';
 
-const endpoint = 'http://localhost:3000';
+const endpoint = 'http://localhost:3000/films/';
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json'
+  })
+};
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class GetMoviesService {
 
   constructor(private http: HttpClient) {
