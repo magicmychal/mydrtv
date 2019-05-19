d098c25c83af90a4ff614b9619ee4f3c31c12b75
Michal P <michalpawlicki@outlook.com>
2019-05-19 16:01:55 +0200

    added child routes, updated authuard in routes, redirect to the home component by default

--- mydrtv/src/app/app-routing.module.ts
+++ mydrtv/src/app/app-routing.module.ts
@@ -1,46 +1,54 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 import {PlayerComponent} from './player/player.component';
 import {FilmComponent} from './film/film.component';
-import { HomeComponent } from './home/home.component';
-import { SignupComponent } from './signup/signup.component';
+import {HomeComponent} from './home/home.component';
+import {SignupComponent} from './signup/signup.component';
 import {LoginComponent} from './login/login.component';
 import {AuthGuardService} from './services/auth-guard.service';
 
 const routes: Routes = [
-  {
-    path: '',
-    redirectTo: 'app',
-    pathMatch: 'full'
-  },
-
-  {
-    path: 'film/:id',
-    component: FilmComponent,
-    // TODO: fix the routing to the player
-  },
-  {
-    path: 'player/:id',
-    component: PlayerComponent,
-    canActivate: [AuthGuardService]
-  },
-  {
-    path: 'home',
-    component: HomeComponent
-  },
-  {
-    path: 'signup',
-    component: SignupComponent
-  },
-  {
-    path: 'login',
-    component: LoginComponent
-  }
+    {
+        path: '',
+        redirectTo: '/home',
+        pathMatch: 'full'
+    },
+    {
+        path: 'film',
+        canActivate: [AuthGuardService],
+        children: [
+            {path: ':id', component: FilmComponent},
+            {path: 'player/:id', component: PlayerComponent}
+        ]
+    },
+    {
+        path: 'player/:id',
+        component: PlayerComponent,
+        canActivate: [AuthGuardService]
+    },
+    {
+        path: 'home',
+        component: HomeComponent,
+        canActivate: [AuthGuardService]
+    },
+    {
+        path: 'signup',
+        component: SignupComponent
+    },
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        path: '**',
+        redirectTo: 'app'
+    }
 ]
 
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule {  }
+export class AppRoutingModule {
+}
