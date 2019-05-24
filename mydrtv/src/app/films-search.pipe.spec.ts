7002b569be1803782d887dc3143435f403884fe7
Renata Rebeca Gavreliuc <renata_girl95@yahoo.com>
2019-05-24 21:33:46 +0200

    temporary data service created & added to pipe specs

--- mydrtv/src/app/films-search.pipe.spec.ts
+++ mydrtv/src/app/films-search.pipe.spec.ts
@@ -1,18 +1,23 @@
 import { FilmsSearchPipe } from './films-search.pipe';
-import {FilmRestService} from "./services/film-rest.service";
+import {TemporaryDataService} from "./temporary-data.service";
 import { Film } from './entities/films';
 
 describe('FilmsSearchPipe', () => {
+  // 1.0: Empty search, should show all films.
+  // 1.1: Search for 'Pusher' (film title), should return one object.
+  // 1.2: Search for 'pusher' (film title), should return one object.
+  // 1.3: Search for 'r' (film title), should return 2 objects.
+  // 1.4: Search for 'abc' (film title), should return empty array
+
   it('create an instance', () => {
     const pipe = new FilmsSearchPipe();
     expect(pipe).toBeTruthy();
   });
 
   it('should return all films when search is empty', () => {
-    // Arrange - Act - Assert (expect)
 
     // Arrange
-    const filmsArray  = FilmRestService.films;
+    const filmsArray  = TemporaryDataService.films;
     const pipe = new FilmsSearchPipe();
 
     // Act
