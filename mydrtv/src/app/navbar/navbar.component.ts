837c99d3d5a6e6da74a061c4bff24fc6875cf0fc
Michal <michalpawlicki@outlook.com>
2019-05-25 13:49:40 +0200

    display current user in the navbar

--- mydrtv/src/app/navbar/navbar.component.ts
+++ mydrtv/src/app/navbar/navbar.component.ts
@@ -8,6 +8,10 @@ import {ActivatedRoute, Router} from '@angular/router';
 import {Store} from '@ngrx/store';
 import * as UserActions from '../redux/user-state-management/user.actions';
 
+import {UserModel} from '../models/user.model';
+import {User} from "../entities/users";
+import {Observable} from "rxjs";
+
 
 @Component({
     selector: 'app-navbar',
@@ -25,12 +29,16 @@ export class NavbarComponent implements OnInit {
         public rest: UsersService,
         private route: ActivatedRoute,
         private router: Router,
-        private store: Store<any>) {
+        private store: Store<UserModel>) {
     }
 
     public usersId: string;
     user: any;
     notFound: string;
+
+    // only for the logged in user
+    currentUser: any;
+    userName: string;
 
     ngOnInit() {
         this.usersId = "5ce1b9264f2e1fa29e4ee216"; //this.route.snapshot.params.id;
@@ -40,6 +48,13 @@ export class NavbarComponent implements OnInit {
             error: err => this.userNotFound(),
             complete: () => console.log('done')
         });
+
+        this.getCurrentUser().subscribe({
+            next: result => this.userName = result.Name,
+            error: error => console.warn('something went wrong with the Observable', error),
+            complete: () => console.log('call finished')
+        });
+        this.currentUser = this.getCurrentUser();
     }
 
     Login() {
@@ -62,5 +77,9 @@ export class NavbarComponent implements OnInit {
         setTimeout(() => {
             this.router.navigate(['/']);
         }, 3000);  //3s
+    }
+
+    getCurrentUser(): Observable<UserModel> {
+        return this.store.select('operations');
     }
 }
