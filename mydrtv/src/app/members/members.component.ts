8dc08b85d2f717118eb26843c046e7674a887401
cjullin <caro2715@stud.kea.dk>
2019-05-25 12:50:09 +0200

    functioning + styled members page

--- mydrtv/src/app/members/members.component.ts
+++ mydrtv/src/app/members/members.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UsersService } from '../services/users.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-members',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./members.component.scss']
 })
 export class MembersComponent implements OnInit {
+  private users;
+  notFound: string;
 
-  constructor() { }
+  constructor(
+    public rest: UsersService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {
+   }
 
   ngOnInit() {
+    this.rest.getUsers().subscribe({
+      next: x => this.users = x,
+      error: err => this.userNotFound(),
+      complete: () => console.log('done')
+    });
   }
-
+  userNotFound() {
+    this.notFound = 'User not found. You will be redirected to the main page in a moment...';
+  }
 }
