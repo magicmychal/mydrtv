714823a02cbf1565e56d4f8bf176a95b1f3baf25
Renata Rebeca Gavreliuc <renata_girl95@yahoo.com>
2019-05-26 23:08:29 +0200

    navbar & welcome component unit tests success

--- mydrtv/src/app/navbar/navbar.component.spec.ts
+++ mydrtv/src/app/navbar/navbar.component.spec.ts
@@ -1,6 +1,12 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NavbarComponent } from './navbar.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClient } from 'selenium-webdriver/http';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterTestingModule } from '@angular/router/testing';
+import { StoreModule } from '@ngrx/store';
+import { filmsReducer } from '../redux/film-state-management/films.reducer';
 
 describe('NavbarComponent', () => {
   let component: NavbarComponent;
@@ -8,6 +14,12 @@ describe('NavbarComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        FormsModule, 
+        RouterTestingModule, 
+        HttpClientModule,
+        StoreModule.forRoot({films: filmsReducer}),
+      ],
       declarations: [ NavbarComponent ]
     })
     .compileComponents();
--- mydrtv/src/app/welcome/welcome.component.spec.ts
+++ mydrtv/src/app/welcome/welcome.component.spec.ts
@@ -1,6 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+
+import {VgCoreModule} from 'videogular2/core';
+import {VgControlsModule} from 'videogular2/controls';
 import { WelcomeComponent } from './welcome.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('WelcomeComponent', () => {
   let component: WelcomeComponent;
@@ -8,7 +12,15 @@ describe('WelcomeComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ WelcomeComponent ]
+      imports:[
+        VgCoreModule,
+        VgControlsModule,
+        BrowserAnimationsModule
+      ],
+      declarations: [ 
+        WelcomeComponent,
+        
+        ]
     })
     .compileComponents();
   }));
