d4610ca51584175da0e91da2d8ed6bd1d4f2a73c
Renata Rebeca Gavreliuc <renata_girl95@yahoo.com>
2019-05-27 15:15:15 +0200

    film component

--- mydrtv/src/app/film/film.component.spec.ts
+++ mydrtv/src/app/film/film.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FilmComponent } from './film.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('FilmComponent', () => {
   let component: FilmComponent;
@@ -8,6 +10,10 @@ describe('FilmComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+        HttpClientModule
+      ],
       declarations: [ FilmComponent ]
     })
     .compileComponents();
