8c8f23a8e9ae6fb83e849563febcef12ad7893f9
Renata Rebeca Gavreliuc <renata_girl95@yahoo.com>
2019-05-27 15:29:39 +0200

    auth-guard service

--- mydrtv/src/app/services/auth-guard.service.spec.ts
+++ mydrtv/src/app/services/auth-guard.service.spec.ts
@@ -1,9 +1,18 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AuthGuardService } from './auth-guard.service';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { StoreModule } from '@ngrx/store';
 
 describe('AuthGuardService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports:[ 
+      HttpClientModule,
+      RouterModule.forRoot([]),
+      StoreModule.forRoot({}),
+    ]
+  }));
 
   it('should be created', () => {
     const service: AuthGuardService = TestBed.get(AuthGuardService);
