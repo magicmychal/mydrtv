b1ade53866419848cbea3891f41ab7cd9c93cac7
Renata Rebeca Gavreliuc <renata_girl95@yahoo.com>
2019-05-27 15:27:48 +0200

    film rest service

--- mydrtv/src/app/services/film-rest.service.spec.ts
+++ mydrtv/src/app/services/film-rest.service.spec.ts
@@ -1,9 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 
 import { FilmRestService } from './film-rest.service';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('FilmRestService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports:[ 
+      HttpClientModule
+    ]
+  }));
 
   it('should be created', () => {
     const service: FilmRestService = TestBed.get(FilmRestService);
